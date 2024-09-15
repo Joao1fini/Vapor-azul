4f763e7151b9ac3dcd4db4ec5cadd991bd78d649
joao1fini <joaovoto86@gmail.com>
2024-09-15 13:54:44 -0300

    base

--- dev.php
+++ dev.php
@@ -10,7 +10,7 @@ include("dev_not.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login e Envio de Imagem</title>
+    <title>Login e Envio de Imagen</title>
     <link rel="stylesheet" href="dev.css">
 </head>
 <body>
